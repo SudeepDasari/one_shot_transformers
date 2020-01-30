cf96a15a678d64263161c419b6d55acbcc6e44fb
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-01-30 14:04:04 -0500

    beginning expert sawyer policy training

---
+++ hem/robosuite/__init__.py
@@ -0,0 +1,41 @@
+def get_env(env_name):
+    if env_name == 'SawyerPickPlace':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlace
+        return SawyerPickPlace
+    if env_name == 'SawyerPickPlaceSingle':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlaceSingle
+        return SawyerPickPlaceSingle
+    if env_name == 'SawyerPickPlaceMilk':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlaceMilk
+        return SawyerPickPlaceMilk
+    if env_name == 'SawyerPickPlaceBread':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlaceBread
+        return SawyerPickPlaceBread
+    if env_name == 'SawyerPickPlaceCereal':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlaceCereal
+        return SawyerPickPlaceCereal
+    if env_name == 'SawyerPickPlaceCan':
+        from hem.robosuite.sawyer.sawyer_pick_place import SawyerPickPlaceCan
+        return SawyerPickPlaceCan
+    if env_name == 'BaxterPickPlace':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlace
+        return BaxterPickPlace
+    if env_name == 'BaxterPickPlaceSingle':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceSingle
+        return BaxterPickPlaceSingle
+    if env_name == 'BaxterPickPlaceMilk':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceMilk
+        return BaxterPickPlaceMilk
+    if env_name == 'BaxterPickPlaceBread':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceBread
+        return BaxterPickPlaceBread
+    if env_name == 'BaxterPickPlaceCereal':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceCereal
+        return BaxterPickPlaceCereal
+    if env_name == 'BaxterPickPlaceCan':
+        from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceCan
+        return BaxterPickPlaceCan
+    raise NotImplementedError
+
+
+from hem.robosuite.gym_wrapper import GymWrapper
--- scripts/train_sawyer_expert.py
+++ scripts/train_sawyer_expert.py
@@ -16,36 +16,50 @@ from ray import tune
 from ray.rllib.utils import try_import_torch
 from ray.tune import grid_search
 from ray.tune.registry import register_env
-from hem.robosuite.baxter.baxter_pick_place import BaxterPickPlaceCan
 from collections import OrderedDict
+import hem.robosuite
+import tensorflow as tf
 
 
-class EnvWrapper(BaxterPickPlaceCan, Env):
-    def __init__(self, config):
-        super().__init__(**config)
-        self.action_space = spaces.Box(-1., 1., shape=(8,), dtype='float32')
-        self.observation_space = OrderedDict()
-        for k, v in self._get_observation().items():
-            self.observation_space[k] = spaces.Box(-np.inf, np.inf, shape=v.shape, dtype='float32')
-        self.observation_space = spaces.Dict(self.observation_space)
+class FCNet(TFModelV2):
+    def __init__(self, obs_space, action_space, num_outputs, model_config,name):
+        super(FCNet, self).__init__(obs_space, action_space, num_outputs, model_config, name)
+        
+        self._hiddens = [128, 128, 64, 32]
+        inputs = tf.keras.layers.Input(shape=(np.product(obs_space.shape),), name="observations")
 
+        fc_top = self._build_fc_tower(inputs, 'fc')
+        layer_out = tf.keras.layers.Dense(
+            num_outputs,
+            name="fc_out",
+            activation=None)(fc_top)
+        
+        value_top = self._build_fc_tower(inputs, 'value')
+        value_out = tf.keras.layers.Dense(
+            1,
+            name="value_out",
+            activation=None)(value_top)
 
-class CustomModel(TFModelV2):
-    """Example of a custom model that just delegates to a fc-net."""
-
-    def __init__(self, obs_space, action_space, num_outputs, model_config,
-                 name):
-        super(CustomModel, self).__init__(obs_space, action_space, num_outputs,
-                                          model_config, name)
-        self.model = FullyConnectedNetwork(obs_space, action_space,
-                                           num_outputs, model_config, name)
-        self.register_variables(self.model.variables())
+        self._action_value_model = tf.keras.Model(inputs, [layer_out, value_out])
+        self._value_out = None
+        self.register_variables(self._action_value_model.variables)
 
     def forward(self, input_dict, state, seq_lens):
-        return self.model.forward(input_dict, state, seq_lens)
+        model_out, self._value_out = self._action_value_model(input_dict["obs_flat"])
+        return model_out, state
 
     def value_function(self):
-        return self.model.value_function()
+        return tf.reshape(self._value_out, [-1])
+
+    def _build_fc_tower(self, inputs, name):
+        i, last_layer = 1, inputs
+        for size in self._hiddens:
+            last_layer = tf.keras.layers.Dense(
+                size,
+                name="{}_{}".format(name, i),
+                activation=tf.nn.relu)(last_layer)
+            i += 1
+        return last_layer
 
 
 if __name__ == "__main__":
@@ -53,24 +67,31 @@ if __name__ == "__main__":
     # register_env("corridor", lambda config: SimpleCorridor(config))
     # \lambda \in [0.9, 1]
 
-    ray.init()
-    ModelCatalog.register_custom_model("my_model", CustomModel)
+    ray.init(local_mode=True)
+    ModelCatalog.register_custom_model("my_model", FCNet)
     tune.run(
         "PPO",
         stop={
-            "timesteps_total": 10000,
+            "timesteps_total": 2000000,
         },
         config={
-            "env": EnvWrapper,  # or "corridor" if registered above
-            "num_workers": 4,
+            "env": "RoboPickPlace",  # or "corridor" if registered above
             "model": {
                 "custom_model": "my_model",
             },
-            "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
-            "sample_async": False,
             "env_config": {
                 "use_camera_obs": False,
-                "reward_shaping": True
+                "reward_shaping": True,
+                "env_name": "SawyerPickPlaceCan",
+                "obs_filters": ['robot-state',  'object-state']
             },
+            "num_workers": 7,
+            "sample_async": False,
+            "lr": 2.5e-4,
+            "vf_loss_coeff": 0.5,
+            "lr_schedule": [(0, 2.5e-4), (100000, 5e-5), (1000000, 1e-5), (4000000, 1e-6)],
+            "lambda": grid_search([0.9, 0.95, 0.99])
         },
+        checkpoint_at_end=True,
+        checkpoint_freq=100000,
     )
