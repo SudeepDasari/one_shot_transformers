4399db3ffa3d14e73917de1ec1544ada917ca56b
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-02-05 17:28:38 -0500

    fixed bug in expert data collection

--- hem/datasets/encoders/trajectory_saver.py
+++ hem/datasets/encoders/trajectory_saver.py
@@ -3,16 +3,33 @@ import cv2
 import copy
 
 
+def _compress_obs(obs):
+    if 'image' in obs:
+        okay, im_string = cv2.imencode('.jpg', obs['image'])
+        assert okay, "image encoding failed!"
+        obs['image'] = im_string
+    return obs
+
+
+def _decompress_obs(obs):
+    if 'image' in obs:
+        obs['image'] = cv2.imdecode(obs['image'], cv2.IMREAD_COLOR)
+    return obs
+
+
 class Trajectory:
     def __init__(self):
         self._data = []
+        self._actions = []
+        self._final_obs = None
     
-    def add(self, obs, reward, done, info):
-        if 'image' in obs:
-            okay, im_string = cv2.imencode('.jpg', obs['image'])
-            assert okay, "image encoding failed!"
-            obs['image'] = im_string
+    def add(self, obs, reward, done, info, action=None):
+        obs = _compress_obs(obs)
         self._data.append(copy.deepcopy((obs, reward, done, info)))
+        self._actions.append(action)
+
+    def log_final(self, obs):
+        self._final_obs = _compress_obs(obs)
 
     @property
     def T(self):
@@ -23,12 +40,15 @@ class Trajectory:
 
     def get(self, t):
         assert isinstance(t, int), "t should be an integer value!"
-        assert 0 <= t < self.T, "index should be in [0, T)"
+        assert 0 <= t < self.T + 1, "index should be in [0, T]"
 
-        data_t = copy.deepcopy(self._data[t])
-        if 'image' in data_t[0]:
-            data_t[0]['image'] = cv2.imdecode(data_t[0]['image'], cv2.IMREAD_COLOR)
-        return data_t
+        if t == self.T:
+            obs_t = _decompress_obs(copy.deepcopy(self._final_obs))
+            return dict(obs=obs_t)
+        
+        obs_t, reward_t, done_t, info_t = copy.deepcopy(self._data[t])
+        obs_t = _decompress_obs(obs_t)
+        return dict(obs=obs_t, reward=reward_t, done=done_t, info=info_t, action=self._actions[t])
 
     def __len__(self):
         return self.T
