8f79d623b72fe0251cba93c98b8495eb937f6778
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-04-15 14:26:30 -0400

    switched to multiple discrim steps per model step

--- hem/models/adversarial_trainer.py
+++ hem/models/adversarial_trainer.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 
 
 class AdversarialTrainer(Trainer):
-
     def _build_optimizer_and_scheduler(self, model):
         if isinstance(model, nn.DataParallel):
             model = model.module
@@ -19,11 +18,12 @@ class AdversarialTrainer(Trainer):
         o1, o2 = optimizers
         
         # calculate player1's gradient and step
-        l1.backward(retain_graph=True)
-        o1.step()
+        if step % self._1_step_per_2 == 0:
+            l1.backward(retain_graph=True)
+            o1.step()
 
         # zero out player1's grad in player2's buffers, then calc player2's grad and step
-        if step % self._config.get('o2_per_1', 1) == 0:
+        if step % self._2_step_per_1 == 0:
             o2.zero_grad()
             l2.backward()
             o2.step()
@@ -35,3 +35,22 @@ class AdversarialTrainer(Trainer):
         l1, l2 = losses
         return l1.item() + l2.item()
 
+    @property
+    def _2_step_per_1(self):
+        o2_per_1 = self._config.get('o2_per_1', 1)
+        if isinstance(o2_per_1, int):
+            return o2_per_1
+        o2_per_1, start = o2_per_1
+        if self.step < start:
+            return 1
+        return o2_per_1
+
+    @property
+    def _1_step_per_2(self):
+        o1_per_2 = self._config.get('o1_per_2', 1)
+        if isinstance(o1_per_2, int):
+            return o1_per_2
+        o1_per_2, start = o1_per_2
+        if self.step < start:
+            return 1
+        return o1_per_2
