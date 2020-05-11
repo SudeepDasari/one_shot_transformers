599dc0c883c26976e0fb667ad2b36c6b079a4524
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-05-11 14:14:12 -0400

    began support for multiple loss functions

--- scripts/train_tc_imitation.py
+++ scripts/train_tc_imitation.py
@@ -12,7 +12,6 @@ import os
 class ImitationModule(nn.Module):
     def __init__(self, embed, config):
         super().__init__()
-        self._ac_bins = config['policy']['ac_bins']
         prior = config['policy'].pop('prior', None)
         if prior == 'sawyer_demos':
             prior = torch.from_numpy(SAWYER_DEMO_PRIOR.astype(np.float32).reshape((1, 1, -1)))
@@ -46,7 +45,13 @@ class ImitationModule(nn.Module):
         else:
             linear, ac = nn.Linear(state_dim + goal_dim, config['policy']['ac_out']), nn.ReLU(inplace=True)
             self._ac_proc = nn.Sequential(linear, ac)
-        self._to_logits = nn.Linear(config['policy']['ac_out'], sum(config['policy']['ac_bins']))
+        
+        self._discrete_actions = config['policy']['discrete_actions']
+        if self._discrete_actions:
+            self._ac_bins = config['policy']['ac_bins']
+            self._to_logits = nn.Linear(config['policy']['ac_out'], sum(config['policy']['ac_bins']))
+        else:
+            raise NotImplementedError
     
     def forward(self, context, images, state):
         context_embed, img_embed = F.normalize(self._embed(context), dim=-1), F.normalize(self._embed(images), dim=-1)
@@ -64,19 +69,24 @@ class ImitationModule(nn.Module):
 
         goal = goal[:, None].repeat((1, state.shape[1], 1))
         state_goal = torch.cat((state, goal), 2)
-        logits = self._to_logits(self._proc_acs(state_goal)) + self._prior
-        ac_logits, bins = [], logits
-        for a in self._ac_bins:
-            ac_logits.append(bins[:,:,:a])
-            bins = bins[:,:,a:]
-        return ac_logits
+        return self._predict_actions(state_goal)
 
-    def _proc_acs(self, state_goal):
+    def _predict_actions(self, state_goal):
         if self._is_lstm:
             self._ac_proc.flatten_parameters()
-            return self._ac_proc(state_goal)[0]
-        return self._ac_proc(state_goal)
-    
+            ac_proc = self._ac_proc(state_goal)[0]
+        else:
+            ac_proc = self._ac_proc(state_goal)
+
+        if self._discrete_actions:
+            logits = self._to_logits(ac_proc) + self._prior
+            ac_logits, bins = [], logits
+            for a in self._ac_bins:
+                ac_logits.append(bins[:,:,:a])
+                bins = bins[:,:,a:]
+            return ac_logits
+        
+
     def pe(self, state):
         st = []
         for d, n_p in enumerate(self._pe):
@@ -119,13 +129,15 @@ if __name__ == '__main__':
         states, images = traj['states'][:,:-1].to(device), traj['images'][:,:-1].to(device)
         actions = traj['actions'].type(torch.long).to(device)
 
-        action_logits = pi(context, images, states)
+        import pdb; pdb.set_trace()
+        predicted_actions = pi(context, images, states)        
+        if isinstance(predicted_actions, list):
+            loss, stats = 0, {}
+            for d, logits in enumerate(predicted_actions):
+                l_d = cross_entropy(logits.view((-1, logits.shape[-1])), actions[:,:,d].view(-1))
+                acc = np.mean(np.argmax(logits.detach().cpu().numpy(), 2) == actions[:,:,d].cpu().numpy())
+                stats['l_{}'.format(d)], stats['acc_{}'.format(d)] = l_d.item(), acc
+                loss = loss + l_d
         
-        loss, stats = 0, {}
-        for d, logits in enumerate(action_logits):
-            l_d = cross_entropy(logits.view((-1, logits.shape[-1])), actions[:,:,d].view(-1))
-            acc = np.mean(np.argmax(logits.detach().cpu().numpy(), 2) == actions[:,:,d].cpu().numpy())
-            stats['l_{}'.format(d)], stats['acc_{}'.format(d)] = l_d.item(), acc
-            loss = loss + l_d
         return loss, stats
     trainer.train(policy, forward)
