c6a5465277d14fe5a77cc301ad8059b4f10f4be9
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-05-15 17:44:07 -0400

    re-wrote goal-state attention module

--- hem/models/traj_embed.py
+++ hem/models/traj_embed.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 import math
+import numpy as np
 
 
 class BaseTraj(nn.Module):
@@ -51,18 +52,38 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-class AttentionGoalState(nn.Module):
-    def __init__(self, in_dim=2048, out_dim=32, max_len=500, n_heads=8, ntrans=2, dropout=0.1, T=10):
+class _NonLocalLayer(nn.Module):
+    def __init__(self, in_dim, out_dim, feedforward_dim=512, dropout=0):
         super().__init__()
-        assert in_dim % n_heads == 0, "n_heads must evenly divide input dimension!"
-        self._pe = PositionalEncoding(in_dim, max_len=max_len, dropout=dropout)
-        self._trans = nn.TransformerEncoder(nn.TransformerEncoderLayer(in_dim, n_heads, dropout=dropout), ntrans)
-        self._goal_conv = nn.Conv1d(in_dim, out_dim, T)
-        self._mh_attn = nn.MultiheadAttention(in_dim, n_heads)
-        self._dpt1, self._dpt2, self._dpt3 = nn.Dropout(dropout), nn.Dropout(dropout), nn.Dropout(dropout)
-        self._n1, self._n2 = nn.LayerNorm(in_dim), nn.LayerNorm(out_dim)
-        self._ac = nn.ReLU(inplace=True)
-        self._l1, self._out = nn.Linear(in_dim, in_dim), nn.Linear(in_dim, out_dim)
+        self._in_dim = in_dim
+        self._K = nn.Conv3d(in_dim, feedforward_dim, 1)
+        self._V = nn.Conv3d(in_dim, feedforward_dim, 1)
+        self._Q = nn.Conv3d(in_dim, feedforward_dim, 1)
+        self._a1, self._drp1 = nn.ReLU(inplace=dropout==0), nn.Dropout3d(dropout)
+        self._out = nn.Conv3d(feedforward_dim, out_dim, 1)
+        self._a2, self._drop2 = nn.ReLU(inplace=dropout==0), nn.Dropout3d(dropout)
+        self._norm = nn.BatchNorm3d(out_dim)
+    
+    def forward(self, inputs):
+        K, Q, V = self._K(inputs), self._Q(inputs), self._V(inputs)
+        B, C, T, H, W = K.shape
+        
+        KQ = torch.matmul(K.reshape((B, C, T*H*W)).transpose(1, 2), Q.reshape((B, C, T*H*W)))
+        attn = F.softmax(KQ / np.sqrt(self._in_dim), 2)
+        V = torch.sum(V.view((B, C, T*H*W, 1)) * attn[:,None], 3).reshape((B, C, T, H, W))
+        V = self._drp1(self._a1(V))
+        return self._norm(inputs + self._drop2(self._a2(self._out(V))))
+
+class AttentionGoalState(nn.Module):
+    def __init__(self, in_dim=1024, out_dim=128, max_pos_len=3000, num_attn=2, dropout=0.0, T=3, ff_dim=512):
+        super().__init__()
+        self._pe = PositionalEncoding(out_dim, max_len=max_pos_len, dropout=dropout)
+        self._nonloc_layers = nn.Sequential(*[_NonLocalLayer(in_dim, in_dim, ff_dim, dropout) for _ in range(num_attn)])
+        self._temporal_pool = nn.Sequential(nn.Conv3d(in_dim, out_dim, T), nn.ReLU(inplace=True))
+        self._spatial_pool = nn.AdaptiveAvgPool3d(1)
+        upsample_goal = [torch.nn.ConvTranspose2d(in_dim, ff_dim, 2, stride=2), nn.ReLU(inplace=True)]
+        upsample_goal.append(torch.nn.ConvTranspose2d(ff_dim, out_dim, 2, stride=2))
+        self._upsample_goal = nn.Sequential(*upsample_goal)
         self._out_dim = out_dim
 
     def forward(self, context, frame):
@@ -70,15 +91,18 @@ class AttentionGoalState(nn.Module):
         context should be [B, T, in_dim]
         frame should be [B, in_dim]
         """
-        ctx_embeds = self._pe(context.transpose(0, 1))
-        ctx_embeds = self._trans(ctx_embeds)
-        goal_embed = torch.mean(self._goal_conv(ctx_embeds.permute((1, 2, 0))), 2)
+        ctx_embeds = self._nonloc_layers(context.transpose(1, 2))
+        goal_embed = self._spatial_pool(self._temporal_pool(ctx_embeds))[:,:,0,0,0]
         
-        state_goal_attn = self._mh_attn(frame.transpose(0, 1), ctx_embeds, ctx_embeds)[0].transpose(0, 1)
-        state_goal_attn = self._n1(self._dpt1(state_goal_attn) + frame)
-        state_goal_attn = self._dpt2(self._ac(self._l1(state_goal_attn))) + frame
-        state_goal_attn = self._n2(self._out(state_goal_attn))
-        return goal_embed, state_goal_attn
+        B, T, C, H, W = frame.shape
+        fr = frame.view((B * T, C, H, W))
+        state_embed = self._upsample_goal(frame.view((B * T, C, H, W))).reshape((B, T, self._out_dim, H*4, W*4))
+        state_embed = state_embed.reshape((B * T, self._out_dim, H * W * 16))
+        V = self._pe(state_embed.permute(2, 0, 1)).transpose(0, 1)
+        
+        SG = torch.matmul(state_embed.transpose(1, 2), goal_embed[:,:,None].repeat((T, 1, 1))) / np.sqrt(self._out_dim)
+        state_goal_attn = torch.sum(torch.softmax(SG, 1) * V, 1)
+        return goal_embed, state_goal_attn.reshape((B, T, self._out_dim))
 
 
 class GoalState(nn.Module):
