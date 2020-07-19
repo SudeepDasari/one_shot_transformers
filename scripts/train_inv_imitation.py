7699d371be8764a02c99fa5574f54d138bd0420d
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-07-19 16:34:34 -0400

    added margin and no goal loss options

--- scripts/train_inv_imitation.py
+++ scripts/train_inv_imitation.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     
     # build Imitation Module and MDN Loss
     pnt_weight = config.get('pnt_weight', 0.1)
+    goal_loss, goal_margin = config.get('goal_loss', True), config.get('goal_margin', -1)
     action_model = InverseImitation(**config['policy'])
     def forward(m, device, context, traj, append=True):
         states, actions = traj['states'].to(device), traj['actions'].to(device)
@@ -30,11 +31,20 @@ if __name__ == '__main__':
         inv_distribution = DiscreteMixLogistic(*out['inverse_distrib'])
         l_inv = torch.mean(-inv_distribution.log_prob(actions))
         
-        # compute goal embedding l2 loss
-        l_goal = torch.mean(torch.sum((out['pred_goal'][:,0] - out['img_embed'][:,-1].detach()) ** 2, 1))
+        # compute goal embedding
+        if not goal_loss:
+            l_goal, goal_stat = 0, 0
+        elif goal_margin < 0:
+            l_goal = torch.mean(torch.sum((out['pred_goal'][:,0] - out['img_embed'][:,-1].detach()) ** 2, 1))
+            goal_stat = l_goal.item()
+        else:
+            cos_sims = torch.matmul(out['pred_goal'], out['img_embed'].transpose(1, 2))
+            goal_sim, other_sim = cos_sims[:,:,-1], cos_sims[:,0,:-1]
+            l_goal = torch.mean(torch.nn.functional.relu(other_sim - goal_sim + goal_margin))
+            goal_stat = l_goal.item()
 
         loss = l_goal + l_inv + l_bc
-        stats = {'inverse_loss':l_inv.item(), 'bc_loss': l_bc.item(), 'goal_loss': l_goal.item()}
+        stats = {'inverse_loss':l_inv.item(), 'bc_loss': l_bc.item(), 'goal_loss': goal_stat}
 
         if 'point_ll' in out:
             l_point = torch.mean(-out['point_ll'][range(pnts.shape[0]), pnts[:,-1,0], pnts[:,-1,1]])
