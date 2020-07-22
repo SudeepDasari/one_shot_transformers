e8c59d47c807d713d763218c5b424b3ab682d987
Sudeep Dasari <sdasari@andrew.cmu.edu>
2020-07-22 18:49:22 -0400

    initial versions of baseline models written

---
+++ hem/models/baseline_module.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from hem.models import get_model
+from hem.models.inverse_module import _DiscreteLogHead
+from hem.models.discrete_logistic import DiscreteMixLogistic
+
+
+class ContextualLSTM(nn.Module):
+    def __init__(self, latent_dim, lstm_out=32, n_mix=2, adim=8, const_var=True, n_lstm_layer=1, dropout=0.2, embed_hidden=256, attn_temp=None, use_attn=True):
+        super().__init__()
+        self._resnet18 = get_model('resnet')(output_raw=True, drop_dim=2, use_resnet18=True)
+        self._to_embed = nn.Sequential(nn.Linear(1024, embed_hidden), nn.Dropout(dropout), nn.ReLU(), nn.Linear(embed_hidden, latent_dim))
+        self._encoder = nn.LSTM(latent_dim, lstm_out, n_lstm_layer)
+        self._decoder = nn.LSTM(latent_dim, lstm_out, n_lstm_layer)
+        self._hidden_2_attn = nn.Sequential(nn.Linear(2 * lstm_out, 2 * lstm_out), nn.ReLU(), nn.Linear(2 * lstm_out, latent_dim))
+        self._action_dist = _DiscreteLogHead(lstm_out, adim, n_mix, const_var)
+        self._temp = attn_temp if attn_temp else 1
+        self._use_attn = use_attn
+
+    def forward(self, states, images, context, ret_dist=True):
+        context_embed = self._embed_images(context)
+        img_embed = self._embed_images(images)
+        
+        _, hidden = self._encoder(context_embed.transpose(0, 1))
+        if self._use_attn:
+            dec_out = []
+            for t in range(img_embed.shape[1]):
+                all_prev = torch.cat((context_embed, img_embed[:,:t+1]), dim=1)
+                attn_query = self._hidden_2_attn(torch.cat((hidden[0][0], hidden[1][0]), 1))
+                attn_weights = torch.matmul(attn_query[:,None], all_prev.transpose(1, 2)) / self._temp
+                attn_weights = F.softmax(attn_weights, dim=2)
+                
+                dec_in = torch.sum(attn_weights.transpose(1, 2) * all_prev, dim=1)
+                out, hidden = self._decoder(dec_in[None], hidden)
+                dec_out.append(out)
+            dec_out = torch.cat(dec_out, 0).transpose(0, 1)
+        else:
+            dec_out, _ = self._decoder(img_embed.transpose(0, 1), hidden)
+            dec_out = dec_out.transpose(0, 1)
+        mu, scale, logit = self._action_dist(dec_out)
+        out = DiscreteMixLogistic(mu, scale, logit) if ret_dist else (mu, scale, logit)
+        return out
+
+    def _embed_images(self, images):
+        features = self._resnet18(images)
+        features = F.softmax(features.reshape((features.shape[0], features.shape[1], features.shape[2], -1)), dim=3).reshape(features.shape)
+        h = torch.sum(torch.linspace(-1, 1, features.shape[3]).view((1, 1, 1, -1)).to(features.device) * torch.sum(features, 4), 3)
+        w = torch.sum(torch.linspace(-1, 1, features.shape[4]).view((1, 1, 1, -1)).to(features.device) * torch.sum(features, 3), 3)
+        return F.normalize(self._to_embed(torch.cat((h, w), 2)), dim=2)
---
+++ scripts/train_contextual_baseline.py
@@ -0,0 +1,33 @@
+import torch
+from hem.models import Trainer
+from hem.models.baseline_module import ContextualLSTM
+from hem.models.discrete_logistic import DiscreteMixLogistic
+import numpy as np
+import matplotlib.pyplot as plt
+from hem.datasets.util import MEAN, STD
+import cv2
+
+
+if __name__ == '__main__':
+    trainer = Trainer('bc_contextual', "Trains Behavior Clone w/ inverse + goal on input data")
+    config = trainer.config
+    
+    # build Imitation Module and MDN Loss
+    action_model = ContextualLSTM(**config['policy'])
+    def forward(m, device, context, traj, append=True):
+        states, actions = traj['states'].to(device), traj['actions'].to(device)
+        images = traj['images'].to(device)
+        context = context.to(device)
+
+        # compute predictions and action LL
+        mu_bc, scale_bc, logit_bc = m(states, images, context, ret_dist=False)
+        action_distribution = DiscreteMixLogistic(mu_bc[:,:-1], scale_bc[:,:-1], logit_bc[:,:-1])
+        l_bc = torch.mean(-action_distribution.log_prob(actions))
+    
+        stats = {'l_bc': l_bc.item()}
+        mean_ac = np.clip(action_distribution.mean.detach().cpu().numpy(), -1, 1)
+        for d in range(actions.shape[2]):
+            a_d = actions.cpu().numpy()[:,:,d]
+            stats['bc_l1_{}'.format(d)] = np.mean(np.abs(mean_ac[:,:,d] - a_d))
+        return l_bc, stats
+    trainer.train(action_model, forward)
